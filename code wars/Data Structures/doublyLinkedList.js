dd9cb091fac80bcdc1ad5799da121d7ef0ea92ea
Sunday Chukwuebuka <ikesundayshield@gmail.com>
2022-05-25 14:13:54 +0100

    DLL push method implementation

--- code wars/Data Structures/doublyLinkedList.js
+++ code wars/Data Structures/doublyLinkedList.js
@@ -20,6 +20,37 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push(val) {
+        /*
+                PSEUDOCODE
+            1. create a new node with the value passed to the function
+            2. if the head is null, set the head and tail to be the newly created node 
+            3. if not, set the next property on the tail to be that node
+            4. set the previous property of the newly created node to be the tail.
+            5. set the tail to be the newly created  node
+            6. increment the length
+            7. return the entire list
+        
+        */
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+let list = new DoublyLinkedList();
+list.push(99);
+list.push(100);
+list.push("last item");
+console.log(list);
 
 
