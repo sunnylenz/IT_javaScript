6c624b1fa23c3820344ac716d97da1ccc9a05b4d
Sunday Chukwuebuka <ikesundayshield@gmail.com>
2022-05-25 16:57:28 +0100

    DLL pop method implementation

--- code wars/Data Structures/doublyLinkedList.js
+++ code wars/Data Structures/doublyLinkedList.js
@@ -45,12 +45,36 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        /*
+                 POPPING PSEUDOCODE
+            1. if there is no head, return undefined
+            2. store the current tail in a variable to return later
+            3. if the lentgh is 1, set the head and tail to be null
+            4. updte the tail to be the previous node
+            5. set the new tails next to null
+        */
+        if (!this.head) return undefined;
+        let poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
 }
 
 let list = new DoublyLinkedList();
-list.push(99);
-list.push(100);
-list.push("last item");
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list.pop());
 console.log(list);
 
 
